bc0e8ee3a36929a398099dde36068edc6d2a2f7e
Jaktlaget <snabb.grek@gmail.com>
2023-09-07 23:01:49 +0200

    TopMenu

---
+++ bowmore/src/App.js
@@ -0,0 +1,12 @@
+import TopMenu from './Widgets/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+          <TopMenu/>
+    </div>
+  );
+}
+
+export default App;
